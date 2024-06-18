e40bb62b8c31f1194e0f4313ec786c0e46b5203b
Franklin-pro <136065526+Franklin-pro@users.noreply.github.com>
2024-06-18 16:51:57 +0200

    ft:create about web page and contact page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Image from "next/image";
 import Page from "./pages";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 export default function Home() {
   return (
   <>
   <Page />
+  <About/>
+  <Contact/>
   </>
   );
 }
